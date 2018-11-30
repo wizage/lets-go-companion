61120f11a0d50a9113f954f47de073380973ca38
wizage <wizagesmax@gmail.com>
2018-11-30 11:12:09 -0800

    Developer v0.0.4 - Cleaned up pokemonview.plist - Added labels and initial cells to pokedex - Fixed crash on pokedex tab

--- Let's go companion/Pokedex/PokedexCollectionViewController.swift
+++ Let's go companion/Pokedex/PokedexCollectionViewController.swift
@@ -8,19 +8,37 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
+private let reuseIdentifier = "cell"
 
 class PokedexCollectionViewController: UICollectionViewController {
     
-    var pokedexArray : Array<Dictionary<String, String>>!
+    var pokedexArray : Array<Dictionary<String, Any>>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let pokedexURL = Bundle.main.url(forResource: "PokedexView", withExtension: "plist")
         let pokedexData = try! Data(contentsOf: pokedexURL!)
-        pokedexArray = try! PropertyListSerialization.propertyList(from: pokedexData, options: [], format: nil) as! Array<Dictionary<String, String>>
+        pokedexArray = try! PropertyListSerialization.propertyList(from: pokedexData, options: [], format: nil) as! Array<Dictionary<String, Any>>
 
+        
+        let screenSize = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        let layout : UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        
+        if (screenWidth > screenHeight){
+            layout.sectionInset = UIEdgeInsets(top: 10, left: (screenHeight - 300)/3, bottom: 10, right: (screenHeight - 300)/3)
+        } else {
+            layout.sectionInset = UIEdgeInsets(top: 10, left: (screenWidth - 300)/3, bottom: 10, right: (screenWidth - 300)/3)
+        }
+        
+        //layout.sectionInset = UIEdgeInsets(top: 10, left: (screenWidth - 300)/3, bottom: 10, right: (screenWidth - 300)/3)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 15
+        layout.itemSize = CGSize(width: 150, height: 150)
+        collectionView!.collectionViewLayout = layout
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -47,12 +65,15 @@ class PokedexCollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
+        //Minus 18 to deal with alolan types
         return pokedexArray.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! EventCollectionViewCell
     
+        cell.titleLabel.text = pokedexArray[indexPath.row]["PokemonName"] as? String
+        
         // Configure the cell
     
         return cell
